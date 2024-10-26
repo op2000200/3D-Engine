234eb780d8a00cad3ed0e6622139d7c33aa56395
op2000200 <op2000200@yandex.ru>
2024-10-26 20:59:05 +0700

    tried to create a lib in cmake, but can not use it in sample

---
+++ Engine/Lib/Include/Window/Window.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace engine
+{
+    namespace window
+    {
+        int add(int a, int b);
+    } // namespace window
+} // namespace engine
---
+++ Engine/Lib/Source/Window.cpp
@@ -0,0 +1,12 @@
+#include <Window/Window.hpp>
+
+namespace engine
+{
+    namespace window
+    {
+        int add(int a, int b)
+        {
+            return a + b;
+        }
+    } //namespace window
+} //namespace engine
